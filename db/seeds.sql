6c62b44e178322e7f176079a26c1e689a923a5e3
Paul Carr <pwcarr15@gmail.com>
2020-12-12 12:50:28 -0500

    Styled webpage more and removed delete button.

--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-USE burger_db;
-
-INSERT INTO burgers (name) VALUES ('Texan');
-INSERT INTO burgers (name) VALUES ('Pimento');
-INSERT INTO burgers (name, tried) VALUES ('Bacon', true);
-INSERT INTO burgers (name, tried) VALUES ('Blue', true);
-INSERT INTO burgers (name, tried) VALUES ('Cheese', true);
-INSERT INTO burgers (name) VALUES ('BBQ');

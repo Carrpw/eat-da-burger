0bb034a3aee0f739f537c49c8235bd9de1693d5e
Paul Carr <pwcarr15@gmail.com>
2020-12-12 10:54:19 -0500

    Initial commit.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers (name) VALUES ('Texan');
+INSERT INTO burgers (name) VALUES ('Pimento');
+INSERT INTO burgers (name, tried) VALUES ('Bacon', true);
+INSERT INTO burgers (name, tried) VALUES ('Blue', true);
+INSERT INTO burgers (name, tried) VALUES ('Cheese', true);
+INSERT INTO burgers (name) VALUES ('BBQ');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,60 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newType = $(this).data("newtype");
+
+    var newTypeState = {
+      tried: newType
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newTypeState
+    }).then(
+      function() {
+        console.log("changed if you've tried it to", newType);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#bur").val().trim(),
+      tried: $("[name=tried]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
